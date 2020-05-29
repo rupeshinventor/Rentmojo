394102ed4f0eb9507ccd60ca5227f80562f2eb0f
rupesh kumar mishra <32673120+rupeshinventor@users.noreply.github.com>
2020-05-29 11:39:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 ?>
 <html>
 	<head>
-		<title>Address Book</title>
+		<title>PhoneBook</title>
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="css/custom.css">
 		<link href="https://fonts.googleapis.com/css?family=Baloo+Bhaina" rel="stylesheet">
